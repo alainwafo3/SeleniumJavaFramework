d7a7490286cbaccd6a30be04eed3c8419eee58b2
WAFO Pascal <alainwafo3@gmail.com>
2021-11-22 11:51:51 +0100

    Ajout de la classe GitHubDemo pour tester

---
+++ SeleniumJavaFramework/src/test/java/demo/GitHubDemo.java
@@ -0,0 +1,7 @@
+package demo;
+
+public class GitHubDemo {
+
+	//https://www.youtube.com/watch?v=s5FAv4jPpDA&list=PLhW3qG5bs-L8oRay6qeS70vJYZ3SBQnFa&index=36
+	//https://stackoverflow.com/questions/30421875/cant-connect-to-any-uri-error-while-commiting-code-from-eclipse-to-git-reposito
+}
